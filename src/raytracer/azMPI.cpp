49b9bdbcc931f78036e2a135d2ae8aca5baf4d83
Xiao Li <xiao.li.cmu@gmail.com>
2014-12-10 20:20:08 -0500

    changed a shadow ray generation logic

--- src/raytracer/azMPI.cpp
+++ src/raytracer/azMPI.cpp
@@ -2,105 +2,108 @@
 #include "MPICommunicate.hpp"
 
 namespace _462 {
-
-  void azMPI::mpiPathTrace()
-  {
-    if (procId == 0)
+    
+    void azMPI::mpiPathTrace()
     {
-      std::vector<Ray> ray_list;
-      raytracer.generateEyeRay(ray_list);
-
-      for (Ray &r: ray_list)
-      {
-        int dest = raytracer.checkNextBoundingBox(r, procId);
-        if (dest == 0)
+        if (procId == 0)
         {
-          raytracer.updateFrameBuffer(r);
-          continue;
+            std::vector<Ray> ray_list;
+            raytracer.generateEyeRay(ray_list);
+            
+            for (Ray &r: ray_list)
+            {
+                int dest = raytracer.checkNextBoundingBox(r, procId);
+                if (dest == 0)
+                {
+                    raytracer.updateFrameBuffer(r);
+                    continue;
+                }
+                MPICommunicate::ISendRay(&r, dest);
+                send_count++;
+            }
+            
+            run_master();
         }
-        MPICommunicate::ISendRay(&r, dest);
-        send_count++;
-      }
-
-      run_master();
+        else
+        {
+            run_slave();
+        }
+        return;
     }
-    else
+    
+    void azMPI::run_master()
     {
-      run_slave();
+        Ray recv_r;
+        while(1)
+        {
+            MPICommunicate::RecvRay(recv_r);
+            recv_count++;
+            switch(recv_r.type)
+            {
+                case ERayType_Eye:
+                {
+                    raytracer.updateFrameBuffer(recv_r);
+                    if (recv_r.isHit)
+                    {
+                        Ray *shadowray = new Ray();
+                        raytracer.generateShadowRay(recv_r, *shadowray);
+                        MPICommunicate::ISendRay(shadowray,
+                                                 raytracer.checkNextBoundingBox(*shadowray, procId));
+                    }
+                    send_count++;
+                }
+                    break;
+                case ERayType_Shadow:
+                    raytracer.updateFrameBuffer(recv_r);
+                    break;
+                case ERayType_GI:
+                    break;
+                case ERayType_Terminate:
+                default:
+                    printf("Proc = %d, Master received a wrong ray type = %x\n", procId, recv_r.type);
+                    exit(-1);
+            }
+            
+            if (send_count == recv_count)
+            {
+                Ray terminate;
+                terminate.type = ERayType_Terminate;
+                MPICommunicate::BroadCast(terminate);
+                break;
+            }
+        }
     }
-    return;
-  }
-
-  void azMPI::run_master()
-  {
-    Ray recv_r;
-    while(1)
+    
+    void azMPI::run_slave()
     {
-      MPICommunicate::RecvRay(recv_r);
-      recv_count++;
-      switch(recv_r.type)
-      {
-        case ERayType_Eye:
-          {
-            raytracer.updateFrameBuffer(recv_r);
-            Ray *shadowray = new Ray();
-            raytracer.generateShadowRay(recv_r, *shadowray);
-            MPICommunicate::ISendRay(shadowray,
-                raytracer.checkNextBoundingBox(*shadowray, procId));
-            send_count++;
-          }
-          break;
-        case ERayType_Shadow:
-          raytracer.updateFrameBuffer(recv_r);
-          break;
-        case ERayType_GI:
-          break;
-        case ERayType_Terminate:
-        default:
-          printf("Proc = %d, Master received a wrong ray type = %x\n", procId, recv_r.type);
-          exit(-1);
-      }
-
-      if (send_count == recv_count)
-      {
-        Ray terminate;
-        terminate.type = ERayType_Terminate;
-        MPICommunicate::BroadCast(terminate);
-        break;
-      }
+        bool finished = false;
+        Ray recv_r;
+        while(!finished)
+        {
+            MPICommunicate::RecvRay(recv_r);
+            printf("received something \n");
+            switch(recv_r.type)
+            {
+                case ERayType_Eye:
+                {
+                    int next_node;
+                    next_node = raytracer.localRaytrace(recv_r);
+                    Ray *send_ray_buf = new Ray(&recv_r);
+                    MPICommunicate::ISendRay(send_ray_buf, next_node);
+                }
+                    break;
+                case ERayType_Shadow:
+                    break;
+                case ERayType_GI:
+                    break;
+                case ERayType_Terminate:
+                    finished = true;
+                    break;
+                default:
+                    printf("Proc = %d, Slave received a wrong ray type = %d\n", procId, recv_r.type);
+                    exit(-1);
+            }
+        }
     }
-  }
-
-  void azMPI::run_slave()
-  {
-    bool finished = false;
-    Ray recv_r;
-    while(!finished)
-    {
-      MPICommunicate::RecvRay(recv_r);
-      printf("received something \n");
-      switch(recv_r.type)
-      {
-        case ERayType_Eye:
-          {
-            int next_node;
-            next_node = raytracer.localRaytrace(recv_r);
-            Ray *send_ray_buf = new Ray(&recv_r);
-            MPICommunicate::ISendRay(send_ray_buf, next_node);
-          }
-          break;
-        case ERayType_Shadow:
-          break;
-        case ERayType_GI:
-          break;
-        case ERayType_Terminate:
-          finished = true;
-          break;
-        default:
-          printf("Proc = %d, Slave received a wrong ray type = %d\n", procId, recv_r.type);
-          exit(-1);
-      }
-    }
-  }
-
+    
 }  //namespace _46
